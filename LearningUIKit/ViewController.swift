9aace1e08bfa9080aebb01147eceba682d780ad9
AstraKlaus <endrew-k@yandex.ru>
2022-07-08 15:40:06 +0400

    TrafficLight done

--- LearningUIKit/ViewController.swift
+++ LearningUIKit/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redFlightButton: UIView!
+    @IBOutlet var yellowFlightButton: UIView!
+    @IBOutlet var greenFlightButton: UIView!
+    
+    @IBOutlet var generalButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redFlightButton.alpha = 0.5
+        redFlightButton.layer.cornerRadius = 55
+        yellowFlightButton.alpha = 0.5
+        yellowFlightButton.layer.cornerRadius = 55
+        greenFlightButton.alpha = 0.5
+        greenFlightButton.layer.cornerRadius = 55
+        generalButton.layer.cornerRadius = 10
     }
-
-
+    
+    @IBAction private func generalPressed() {
+        if redFlightButton.alpha == 0.5 && yellowFlightButton.alpha == 0.5 && greenFlightButton.alpha == 0.5{
+            redFlightButton.alpha = 1
+        }
+        else if redFlightButton.alpha == 1{
+            redFlightButton.alpha = 0.5
+            yellowFlightButton.alpha = 1
+        }
+        else if yellowFlightButton.alpha == 1{
+            yellowFlightButton.alpha = 0.5
+            greenFlightButton.alpha = 1
+        }
+        else if greenFlightButton.alpha == 1{
+            greenFlightButton.alpha = 0.5
+            redFlightButton.alpha = 1
+        }
+    }
+    
 }
 
