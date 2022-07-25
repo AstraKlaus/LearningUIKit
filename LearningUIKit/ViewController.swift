39c636a8ed76910183ff3ec305a97312c0549586
AstraKlaus <endrew-k@yandex.ru>
2022-07-25 18:56:23 +0400

    rgbColorApp

--- LearningUIKit/ViewController.swift
+++ LearningUIKit/ViewController.swift
@@ -9,39 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet var redFlightButton: UIView!
-    @IBOutlet var yellowFlightButton: UIView!
-    @IBOutlet var greenFlightButton: UIView!
+    @IBOutlet var colorBoardView: UIView!
     
-    @IBOutlet var generalButton: UIButton!
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        redFlightButton.alpha = 0.5
-        redFlightButton.layer.cornerRadius = 55
-        yellowFlightButton.alpha = 0.5
-        yellowFlightButton.layer.cornerRadius = 55
-        greenFlightButton.alpha = 0.5
-        greenFlightButton.layer.cornerRadius = 55
-        generalButton.layer.cornerRadius = 10
+        colorBoardView.layer.cornerRadius = 10
+        redLabel.text = String(format: "%.2f", redSlider.value)
+        blueLabel.text = String(format: "%.2f", blueSlider.value)
+        greenLabel.text = String(format: "%.2f", greenSlider.value)
+        colorBoardView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
     }
     
-    @IBAction private func generalPressed() {
-        if redFlightButton.alpha == 0.5 && yellowFlightButton.alpha == 0.5 && greenFlightButton.alpha == 0.5{
-            redFlightButton.alpha = 1
-        }
-        else if redFlightButton.alpha == 1{
-            redFlightButton.alpha = 0.5
-            yellowFlightButton.alpha = 1
-        }
-        else if yellowFlightButton.alpha == 1{
-            yellowFlightButton.alpha = 0.5
-            greenFlightButton.alpha = 1
-        }
-        else if greenFlightButton.alpha == 1{
-            greenFlightButton.alpha = 0.5
-            redFlightButton.alpha = 1
-        }
+
+    @IBAction func rgbSlidersAction() {
+        redLabel.text = String(format: "%.2f", redSlider.value)
+
+        greenLabel.text = String(format: "%.2f", greenSlider.value)
+
+        blueLabel.text = String(format: "%.2f", blueSlider.value)
+        
+        colorBoardView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
     }
     
 }
