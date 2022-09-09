99b716d5638a3a8b170b45691e2e77915f45916c
AstraKlaus <endrew-k@yandex.ru>
2022-09-09 22:18:09 +0400

    add MainView and delegate

--- LearningUIKit/MainViewController.swift
+++ LearningUIKit/MainViewController.swift
@@ -7,13 +7,36 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+protocol SettingsViewControllerDelegate{
+    func setNewColor(red: CGFloat, green: CGFloat, blue: CGFloat)
+}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
+class MainViewController: UIViewController {
+    
+    var redValue: CGFloat = 255
+    var greenValue: CGFloat = 255
+    var blueValue: CGFloat = 255
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let settingsVC = segue.destination as? SettingsViewController else {return}
+        settingsVC.redValue = redValue
+        settingsVC.greenValue = greenValue
+        settingsVC.blueValue = blueValue
+        settingsVC.delegate = self
     }
     
     @IBAction func unwind(for segue: UIStoryboardSegue){
     }
 }
+
+// MARK: - SettingsViewControllerDelegate
+extension MainViewController: SettingsViewControllerDelegate{
+    func setNewColor(red: CGFloat, green: CGFloat, blue: CGFloat) {
+        view.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        redValue = red
+        greenValue = green
+        blueValue = blue
+        view.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+    }
+}
+
--- LearningUIKit/SettingsViewController.swift
+++ LearningUIKit/SettingsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SettingsViewController.swift
 //  LearningUIKit
 //
 //  Created by Андрей Касьянов on 04.07.2022.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SettingsViewController: UIViewController {
 
     @IBOutlet var colorBoardView: UIView!
     
@@ -19,13 +19,26 @@ class ViewController: UIViewController {
     @IBOutlet var greenLabel: UILabel!
     @IBOutlet var blueLabel: UILabel!
     
+    var redValue: CGFloat!
+    var greenValue: CGFloat!
+    var blueValue: CGFloat!
+    
+    var delegate: SettingsViewControllerDelegate!
+    var backColor: UIColor!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         colorBoardView.layer.cornerRadius = 10
-        redLabel.text = String(format: "%.2f", redSlider.value)
-        blueLabel.text = String(format: "%.2f", blueSlider.value)
-        greenLabel.text = String(format: "%.2f", greenSlider.value)
-        colorBoardView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+        
+        redLabel.text = String(format: "%.2f", Float(redValue))
+        greenLabel.text = String(format: "%.2f", Float(greenValue))
+        blueLabel.text = String(format: "%.2f", Float(blueValue))
+        
+        redSlider.value = Float(redValue)
+        greenSlider.value = Float(greenValue)
+        blueSlider.value = Float(blueValue)
+        
+        colorBoardView.backgroundColor = UIColor(red: redValue, green: greenValue, blue: blueValue, alpha: 1)
     }
     
     @IBAction func rgbSlidersAction() {
@@ -37,6 +50,8 @@ class ViewController: UIViewController {
         
         colorBoardView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
     }
-    
+    @IBAction func doneButtonPressed() {
+        delegate.setNewColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value))
+    }
 }
 
